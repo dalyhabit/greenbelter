928949543dacc54918e227ba3dbf27ed0f834d51
Patrick Daly <dalyhabit@gmail.com>
2017-07-18 10:35:11 -0500

    Add catch-all route

--- server/index.js
+++ server/index.js
@@ -3,6 +3,24 @@ var express = require('express');
 var path = require('path');
 var routes = express.Router();
 
+function waterStatus (flow, depth) {
+  if (flow < 10) {
+    return "Too slow"
+  } else if (flow <= 250) {
+    return "Swimmable"
+  } else if (flow <=325) {
+    return "Not swimmable, but maybe floatable"
+  } else {
+    return "Turn around, don't drown"
+  }
+}
+
+//
+// Static assets (html, etc.)
+//
+var assetFolder = Path.resolve(__dirname, '../client/');
+routes.use(express.static(assetFolder));
+
 routes.get('/usgs', function(req, res){
   request('https://waterservices.usgs.gov/nwis/iv/?format=json&indent=on&sites=08155200,%2008155240,%2008155300,%2008155400,%2008155500&parameterCd=00060,00065&siteStatus=all', function (error, response, body) {
     var dataObj = {
@@ -44,15 +62,35 @@ routes.get('/usgs', function(req, res){
   });
 });
 
-function waterStatus (flow, depth) {
-  if (flow < 10) {
-    return "Too slow"
-  } else if (flow <= 250) {
-    return "Swimmable"
-  } else if (flow <=325) {
-    return "Not swimmable, but maybe floatable"
-  } else {
-    return "Turn around, don't drown"
-  }
-}
 
+if (process.env.NODE_ENV !== 'test') {
+  //
+  // The Catch-all Route
+  // This is for supporting browser history pushstate.
+  // NOTE: Make sure this route is always LAST.
+  //
+  routes.get('/*', function(req, res){
+    res.sendFile( assetFolder + '/index.html' );
+  })
+
+  //
+  // We're in development or production mode;
+  // create and run a real server.
+  //
+  var app = express();
+
+  // Parse incoming request bodies as JSON
+  app.use( require('body-parser').json() );
+
+  // Mount our main router
+  app.use('/', routes);
+
+  // Start the server!
+  var port = process.env.PORT || 4000
+  app.listen(port);
+  console.log("Listening on port", port);
+}
+else {
+  // We're in test mode; make this file importable instead.
+  module.exports = routes
+}
