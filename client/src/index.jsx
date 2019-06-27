b2bda9ae13c83880281b091c960f13b948ffa28b
Patrick Daly <pdaly@me.com>
2019-06-27 14:38:58 -0500

    Work on converting to modals

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,24 +1,78 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import SwimmingHole from './components/SwimmingHole.jsx';
 import GreenbeltMap from './components/GreenbeltMap.jsx';
-import Toolbar from './components/Toolbar.jsx';
+import Modal from 'react-modal';
+
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)'
+  }
+};
+
+Modal.setAppElement('#app');
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      modalIsOpen: false,
+      modalType: null,
+      modalName: null
+    };
+
+    this.openModal = this.openModal.bind(this);
+    this.afterOpenModal = this.afterOpenModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+  }
+
+  openModal(type, name) {
+    console.log('TYPE:', type);
+    console.log('NAME:', name);
+    this.setState({ 
+      modalIsOpen: true,
+      modalType: type,
+      modalName: name
+    });
+  }
+
+  afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    // this.subtitle.style.color = '#f00';
+  }
+
+  closeModal() {
+    this.setState({
+      modalIsOpen: false,
+      modalType: null,
+      modalName: null
+    });
   }
 
   render () {
     return (
-      <Router>
         <div className="react-root">
-          <Toolbar/>
-          <Route exact path="/" component={GreenbeltMap} />
-          <Route path="/details" component={SwimmingHole} />
+          <GreenbeltMap openModal={this.openModal}/>
+          <Modal
+            isOpen={this.state.modalIsOpen}
+            onAfterOpen={this.afterOpenModal}
+            onRequestClose={this.closeModal}
+            style={customStyles}
+            contentLabel="Example Modal"
+          >
+            <div className="modal-header"></div>
+            <div className="flex-container">
+              <div className="google-map"></div>
+              <div className="description"></div>
+            </div>
+          </Modal>
         </div>
-      </Router>
     );
   }
 }
