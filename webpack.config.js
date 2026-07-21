12c3d5d74534812c73a9c5eef2ae3f72feba8b96
Patrick Daly <dalyhabit@gmail.com>
2026-07-20 21:47:38 -0500

    feat: refactor to fix stale html/css being served on prod

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const SRC_DIR = path.join(__dirname, '/client/src');
 const DIST_DIR = path.join(__dirname, '/client/dist');
+const PUBLIC_DIR = path.join(__dirname, '/client/public');
 
 module.exports = {
   mode: process.env.NODE_ENV || 'development',
@@ -8,8 +11,17 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: DIST_DIR,
-    publicPath: '/'
+    publicPath: '/',
+    clean: true
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: `${SRC_DIR}/index.html`
+    }),
+    new CopyWebpackPlugin({
+      patterns: [{ from: PUBLIC_DIR, to: DIST_DIR }]
+    })
+  ],
   module: {
     rules: [
       {
